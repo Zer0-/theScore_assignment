c29c7569770b7a855c4da54f7c1248e870ebf8ae
Philipp Volguine <phil.volguine@gmail.com>
2020-03-11 19:10:09 -0400

    Dump data into BootstrapTable

--- src/components/Rushing.js
+++ src/components/Rushing.js
@@ -1,13 +1,68 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { get } from '../network';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const schema = [{
+        dataField: 'Player',
+        text: 'Player'
+    }, {
+        dataField: 'Team',
+        text: 'Team'
+    }, {
+        dataField: 'Pos',
+        text: 'Position'
+    }, {
+        dataField: 'Att',
+        text: 'Rushing Attempts'
+    }, {
+        dataField: 'Att/G',
+        text: 'Rushing Attempts Per Game Average'
+    }, {
+        dataField: 'Yds',
+        text: 'Total Yards'
+    }, {
+        dataField: 'Avg',
+        text: 'Average Yards Per Attempt'
+    }, {
+        dataField: 'Yds/G',
+        text: 'Yards Per Game'
+    }, {
+        dataField: 'TD',
+        text: 'Total Touchdowns'
+    }, {
+        dataField: 'Lng',
+        text: 'Longest Rush'
+    }, {
+        dataField: '1st',
+        text: 'First Downs'
+    }, {
+        dataField: '1st%',
+        text: 'First Down Percentage'
+    }, {
+        dataField: '20+',
+        text: '20+ Yards'
+    }, {
+        dataField: '40+',
+        text: '40+ Yards'
+    }, {
+        dataField: 'Fum',
+        text: 'Fumbles'
+    }];
 
 function Rushing() {
+    const [records, setRecords] = useState([]);
+
     get('/rushing.json')
-        .then(x => console.log(x));
+         // add integer index column
+        .then(xs => xs.map((x, i) => Object.assign(x, {id: i})))
+        .then(setRecords);
 
     return (
-        <h1>Hello Rushing</h1>
+        <BootstrapTable
+            keyField="id"
+            data={records}
+            columns={schema}
+        />
     );
 }
 
