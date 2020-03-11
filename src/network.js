ef4e805a11ff22b12fa5728eb2f0b4093b2cb211
Philipp Volguine <phil.volguine@gmail.com>
2020-03-11 19:00:09 -0400

    Assume data is json when performing a GET
    - get function in network now parses the response

--- src/network.js
+++ src/network.js
@@ -1,7 +1,17 @@
+function resolveResponse(resolve, reject) {
+    return e => {
+        if (e.target.status >= 200 && e.target.status < 300) {
+            return resolve(JSON.parse(e.target.responseText));
+        } else {
+            reject(new Error('API responded with ' + e.target.status));
+        }
+    }
+}
+
 export function get(url) {
     return new Promise((resolve, reject) => {
         let req = new XMLHttpRequest();
-        req.onload = resolve;
+        req.onload = resolveResponse(resolve, reject);
         req.onerror = reject;
         req.open('GET', url);
         req.setRequestHeader("Accept", "application/json");
